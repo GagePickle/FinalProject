7d4ebfad5c405c61d15e97c58a912ff9ab6f3e66
Gage Pickle <gagepickle@yahoo.com>
2020-12-01 17:24:14 -0600

    Adapt lib.js to fit the project

--- lib.js
+++ lib.js
@@ -3,12 +3,12 @@ const template = document.querySelector("template");
 
 export default (data) => {
   data.forEach(({ name, score, id }) => {
-    const newStudentRow = template.content.cloneNode(true);
-    const newStudentTDs = newStudentRow.querySelectorAll("td");
+    const newItemRow = template.content.cloneNode(true);
+    const newItemTDs = newItemRow.querySelectorAll("td");
 
-    newStudentTDs[0].textContent = id;
-    newStudentTDs[1].textContent = name;
-    newStudentTDs[2].textContent = score;
-    tbody.appendChild(newStudentRow);
+    newItemTDs[0].textContent = id;
+    newItemTDs[1].textContent = name;
+    newItemTDs[2].textContent = score;
+    tbody.appendChild(newItemRow);
   });
 };
